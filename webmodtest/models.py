faa0ed61f6e04d99d7aa634b0a0153c2274aec04
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-02-15 06:38:51 -0800

    Deleted webmodtest folder

--- webmodtest/models.py
+++
@@ -1,9 +0,0 @@
-from google.appengine.ext import db
-
-class Level(db.Model):
-	uid = db.IntegerProperty(required = True)
-	level_name = db.StringProperty(required = True)
-	live_level_data = db.TextProperty()
-	draft_level_data = db.TextProperty()
-	created = db.DateTimeProperty(auto_now_add = True)
-	modified = db.DateTimeProperty(auto_now_add = True)
