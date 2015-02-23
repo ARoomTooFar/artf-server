aa0385acf1a8950e25864aa08835318f19884468
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-02-22 16:45:47 -0800

    modified property actually updates when entity is updated

--- api/models.py
+++ api/models.py
@@ -4,7 +4,7 @@ class Machine(db.Model):
 	mach_name = db.StringProperty(required = True)
 	venue_name = db.StringProperty(required = True)
 	created = db.DateTimeProperty(auto_now_add = True)
-	modified = db.DateTimeProperty(auto_now_add = True)
+	modified = db.DateTimeProperty(auto_now = True)
 
 class Level(db.Model):
 	live_level_data = db.TextProperty()
@@ -13,14 +13,14 @@ class Level(db.Model):
 	game_acct_id = db.IntegerProperty(required = True)
 	mach_id = db.IntegerProperty(required = True)
 	created = db.DateTimeProperty(auto_now_add = True)
-	modified = db.DateTimeProperty(auto_now_add = True)
+	modified = db.DateTimeProperty(auto_now = True)
 
 class GameAccount(db.Model):
 	game_acct_name = db.StringProperty(required = True)
 	game_acct_password = db.StringProperty(required = True)
 	web_acct_id = db.IntegerProperty()
 	created = db.DateTimeProperty(auto_now_add = True)
-	modified = db.DateTimeProperty(auto_now_add = True)
+	modified = db.DateTimeProperty(auto_now = True)
 
 class Character(db.Model):
 	char_name = db.StringProperty(required = True)
@@ -28,7 +28,7 @@ class Character(db.Model):
 	game_acct_id = db.IntegerProperty(required = True)
 	money = db.IntegerProperty(required = True)
 	created = db.DateTimeProperty(auto_now_add = True)
-	modified = db.DateTimeProperty(auto_now_add = True)
+	modified = db.DateTimeProperty(auto_now = True)
 
 class Item(db.Model):
 	item_num = db.IntegerProperty()
@@ -40,5 +40,5 @@ class Item(db.Model):
 	mod_luck = db.IntegerProperty()
 	char_id = db.IntegerProperty(required = True)
 	created = db.DateTimeProperty(auto_now_add = True)
-	modified = db.DateTimeProperty(auto_now_add = True)
+	modified = db.DateTimeProperty(auto_now = True)
 	# still need to account for storing abilities
